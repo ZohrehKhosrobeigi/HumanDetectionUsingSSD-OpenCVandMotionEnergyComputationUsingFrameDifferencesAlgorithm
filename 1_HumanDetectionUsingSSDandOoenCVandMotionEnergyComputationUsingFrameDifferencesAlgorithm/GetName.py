71873e53b49ba7c6aa0abd4a328397e544adf4c8
Zohreh Khosrobeigi <khosrobz@tcd.ie>
2024-02-29 10:42:14 +0000

    a

--- 1_HumanDetectionUsingSSDandOoenCVandMotionEnergyComputationUsingFrameDifferencesAlgorithm/GetName.py
+++
@@ -1,9 +0,0 @@
-class GetName():
-    def getName(self,file):
-        tmp=file.split("/")
-        tmp=tmp[1].split("_")
-        self.player1=tmp[0]
-
-        self.session=tmp[1]
-
-        return self.player1,self.session
